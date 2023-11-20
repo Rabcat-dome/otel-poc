27786b807def11b211a3eb5de034254c8a863b3b
VISARUTP <visarut.p@pttdigital.com>
2023-11-21 00:26:09 +0700

    OpenTelemetry in React https://www.asserts.ai/blog/monitoring-react-opentelemetry/ https://developers.redhat.com/articles/2023/03/22/how-enable-opentelemetry-traces-react-applications#10_step_opentelemetry_demonstration

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Resource } from '@opentelemetry/resources';
 import './index.css'
+
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+import { BatchSpanProcessor, TracerConfig, WebTracerProvider,SimpleSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+
+const collectorOptions = {
+  url: 'http://host.docker.internal:4318/v1/traces', // <-- nginx with reverse proxy to http://localhost:4318/
+};
+
+const collectorExporter = new OTLPTraceExporter({
+
+  headers: {}
+
+});
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'frontend-react',
+  }),
+};
+
+const provider = new WebTracerProvider(providerConfig);
+provider.addSpanProcessor(
+  new BatchSpanProcessor(new OTLPTraceExporter(collectorOptions)),
+);
+//provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+//provider.addSpanProcessor(new SimpleSpanProcessor(collectorExporter));
+provider.addSpanProcessor(new SimpleSpanProcessor(collectorExporter));
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+const fetchInstrumentation = new FetchInstrumentation({});
+fetchInstrumentation.setTracerProvider(provider);
+
+registerInstrumentations({
+  instrumentations: [
+    // getWebAutoInstrumentations initializes all the package.
+	// it's possible to configure each instrumentation if needed.
+  fetchInstrumentation,
+  new HttpInstrumentation(),
+  ],
+  tracerProvider: provider
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
